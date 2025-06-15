5da44b060e790a1a2e235052dd0d2f45bd5fa90e
Ch Lokesh <lokeshchirumamilla2104@gmail.com>
2025-06-15 16:35:52 +0530

    updated to-do

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
-import { TodoWrapper } from './components/TodoWrapper';
-
+import { TodoWrapperLocalStorage } from './components/TodoWrapperLocalStorage';
 function App() {
   return (
     <div className="App">
-      <TodoWrapper  />
+      <TodoWrapperLocalStorage/>
     </div>
   );
 }
